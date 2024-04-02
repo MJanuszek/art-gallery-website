d5ee0082668f9a2b126ef7d2e7359c427dd2eef2
Monika Januszek <monika.januszek.mail@gmail.com>
2024-04-02 15:18:53 +0200

    Header & Introduction added
    with animation effects

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,20 @@
 // import { useState } from "react";
 import "./styles/Home.scss";
+import Introduction from "./Introduction";
 
 function Home() {
-  return <div className="main-page">Welcome to out gallery</div>;
+  return (
+    <div className="main-page">
+      <div className="header">
+        <h1 className="header-title">Welcome to our gallery</h1>
+      </div>
+      <Introduction />
+      <div className="current-events">events</div>
+      <div className="footer">
+        Copyright Monika Januszek. Visit my Github: https://github.com/MJanuszek{" "}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/Introduction.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./styles/Introduction.scss";
+
+function Introduction() {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
+
+  //   -------------------
+  return (
+    <div
+      ref={ref}
+      className={`introduction ${isVisible ? "introduction-animate" : ""}`}
+    >
+      <h1 className="introduction-title">introduction</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        cumque, delectus nisi recusandae ipsum, veniam eligendi et laudantium,
+        quis repudiandae repellat nesciunt autem? Odit dolorem sapiente non
+        neque unde consequuntur. Commodi impedit quis maxime ipsam excepturi
+        nostrum nihil earum vel. Cupiditate nam fugit sit ut sint eum
+        dignissimos eligendi ex recusandae dolores, odio quos quae ad ratione.
+        Fugit repellendus, voluptatum ad, eaque ipsum eos accusantium quod,
+        animi atque facere labore!
+      </div>
+    </div>
+  );
+}
+
+export default Introduction;
