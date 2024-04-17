41d7acdda33d6d758710baa2ad00914deddb9e9a
Monika Januszek <monika.januszek.mail@gmail.com>
2024-04-17 14:33:42 +0200

    Working on Events and Education sections

--- src/Education.jsx
+++ src/Education.jsx
@@ -2,10 +2,13 @@ import "./styles/Education.scss";
 
 function Education() {
   return (
-    <div className="education-section">
-      <div className="edu-offer edu-schools">For Schools</div>
-      <div className="edu-offer edu-seniors">For Seniors</div>
-      <div className="edu-offer edu-individual">For Companies</div>
+    <div className="edu-section">
+      <h1 className="education-title">Learn new things with us</h1>
+      <div className="education-section">
+        <div className="edu-offer edu-schools">For Schools</div>
+        <div className="edu-offer edu-seniors">For Seniors</div>
+        <div className="edu-offer edu-individual">For Companies</div>
+      </div>
     </div>
   );
 }
--- src/Events.jsx
+++ src/Events.jsx
@@ -4,12 +4,17 @@ function Events() {
   return (
     <>
       <div className="latest-events">
-        <h1 className="events-title">Exhibitions</h1>
+        <h1 className="events-title">Events</h1>
         <div className="event"></div>
         <div className="event"></div>
         <div className="event"></div>
       </div>
-      <div className="events-gallery"></div>
+      <h1 className="offers-title">What can we offer</h1>
+      <div className="events-gallery">
+        <div className="img img1">Our Shop</div>
+        <div className="img img2">Membership</div>
+        <div className="img img3">For Press</div>
+      </div>
     </>
   );
 }
